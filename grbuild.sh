b06ffae5af0be22cc1bd6c3a1f74ba5b5b020567
Guenther Raidl <raidl@ac.tuwien.ac.at>
2020-09-09 09:00:19 +0200

    grbuild.sh corrected

--- grbuild.sh
+++ grbuild.sh
@@ -6,7 +6,7 @@
 # pwsh -Command "(gc -en UTF8NoBOM install.rdf) -replace 'pre%oldRev%'.trim(), 'pre%quickFoldersRev%' | Out-File install.rdf"
 # pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%', 'pre%quickFoldersRev%' | Out-File manifest.json"
 # rem "C:\Program Files\7-Zip\7z" a -xr!.svn quickFolders.zip install.rdf chrome.manifest chrome defaults license.txt
-zip -r qf.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt
+zip -r qf.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt qf-background.js release-notes.html
 mv qf.zip qf.xpi
 #"C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersWeb.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt
 #echo %quickFoldersRev% > revision.txt
